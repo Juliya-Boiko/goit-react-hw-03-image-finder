da49330ea1149f00035c8051c75c78fb28fc4d42
Juliya <BoikoJuliya2707@gmail.com>
2022-07-07 18:42:23 +0300

    load more btn

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -18,7 +18,7 @@ export class ImageGallery extends Component {
 
     componentDidUpdate(prevProps, prevState) {
         if (prevProps.query !== this.props.query || prevState.page !== this.state.page) {
-            this.setState({ status: 'loading', page: 1, hits: [], totalHits: null });
+            this.setState({ status: 'loading' });
             imgParams.q = this.props.query;
             imgParams.page = this.state.page;
             getImages(imgParams).then((response) => {
@@ -47,7 +47,11 @@ export class ImageGallery extends Component {
         if (status === 'resolved') {
             return (
                 <div>
-                    <ul>
+                    <ul
+                        style={{
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                        }}>
                         {hits.map((item) => {
                             return <li key={item.id}>
                                 <img src={item.webformatURL} alt="" width="100px" />
@@ -60,76 +64,3 @@ export class ImageGallery extends Component {
         }
     }
 }
-
-// export class ImageGallery extends Component {
-
-//     state = {
-//         data: null,
-//         totalHits: null,
-//         status: 'idle',
-//         // page: 1,
-//     }
-
-//     handleLoadMoreBtn = (evt) => {
-//         console.log(evt);
-//         // this.setState((prevState) => {
-//         //     console.log(prevState.page);
-//         //     // return {
-//         //     //     page: prevState.page + 1,
-//         //     // }
-//         // })
-//     }
-
-//     componentDidUpdate(prevProps, prevState) {
-//         if (prevProps.query !== this.props.query) {
-
-//             this.setState({ status: 'loading', });
-
-//             imgParams.q = this.props.query;
-//             // imgParams.page = this.state.page;
-
-//             setTimeout(() => {
-//                 getImages(imgParams)
-//                     .then((result) => {
-//                         this.setState({ data: result.data.hits, totalHits: result.data.totalHits, status: 'resolved',});
-//                     });
-//             }, 2000);
-        
-//         }
-//     }
-
-    
-
-//     render() {
-//         const { data, totalHits, status } = this.state;
-
-//         if (status === 'idle') {
-//             return <p>Enter a word</p>;
-//         }
-//         if (status === 'loading') {
-//             return <div>Loading...</div>;
-//         }
-//         // if (status === 'resolved' && data.length === 0) {
-//         //     return <p>empty array</p>
-//         // }
-//         if (status === 'resolved' && totalHits > 12) {
-//             return (
-//                 <div>
-//                     <ul>
-//                         {data.map((item) => {
-//                             return <li key={item.id}>
-//                                 <img src={item.webformatURL} alt="" width="100px" />
-//                             </li>
-//                         })}
-//                     </ul>
-//                     <button type="button" onClick={() => {
-//                         this.setState(prevState => {
-//                             return {
-//                             page: prevState.page + 1,
-//                         }
-//                     })} }>Load more</button>
-//                 </div>
-//             );
-//         }
-//     }
-// }
